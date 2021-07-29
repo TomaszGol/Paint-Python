f731f8f7d2e31e1332e0142e8581c52d6a4970fa
TomaszGol <oplane2000@gmail.com>
2021-07-29 12:05:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+
+SCREEN_WIDTH = 1000
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BLUE = (0, 255, 0)
+GREEN = (0, 0, 255)
+
+WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
+
+
+
+pygame.quit()
